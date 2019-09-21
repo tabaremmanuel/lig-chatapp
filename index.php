3cb0e35330926a387c5aac7056f76f932e6269a6
tabaremmanuel <tabar.emmanuel@gmail.com>
2019-09-21 12:03:18 +0800

    Login UI

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>LIG Chat App</title>
+    <link rel="stylesheet" href="./css/master.min.css">
+  </head>
+  <body>
+    <div class="app">
+      <div class="app-header">
+        <div class="container">
+          <h1>Chat app</h1>
+        </div>
+      </div>
+      <div class="login">
+        <div class="container">
+          <form action="index.html" method="post">
+            <input type="text" name="" value="" placeholder="User name">
+            <input type="password" name="" value="" placeholder="password">
+            <input type="submit" name="" value="Sign up / Log in">
+            <p>
+              By signing up, you agree to the Terms of Service and Privacy Policy, including Cookie Use. Others will be able to find you by searching for your email address or phone number when provided.
+            </p>
+          </form>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
